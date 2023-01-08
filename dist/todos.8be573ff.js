90ffb02fd67d450fe699493ceb2f411e55c6e489
kwonsuhyuk <tngur0716@hanyang.ac.kr>
2023-01-09 04:41:47 +0900

    새로고침시 포스트잇 위치 수정해야함

--- dist/todos.8be573ff.js
+++ dist/todos.8be573ff.js
@@ -128,7 +128,7 @@ var toDos = [];
 var submitRed = todoForm.querySelector(".btn-red");
 var submitBlue = todoForm.querySelector(".btn-blue");
 var submitGreen = todoForm.querySelector(".btn-green");
-var postitColor;
+var postitColor = "red";
 var icon = document.createElement("i");
 icon.setAttribute("class", "fa-solid fa-check fa-2xl");
 submitRed.addEventListener("click", function () {
@@ -158,8 +158,13 @@ function deleteTodo(event) {
   saveToDos();
 }
 
+// 좌표 저장용
+var initX = 0;
+var initY = 10;
+
 // 포스트잇 제작
-function makePostit(newTodoObj) {
+function makePostit(newTodoObj, ifNew) {
+  var postContainer = document.createElement("div");
   var postit = document.createElement("div");
   var dot = document.createElement("span");
   var textLine = document.createElement("div");
@@ -175,7 +180,77 @@ function makePostit(newTodoObj) {
   postit.appendChild(dot);
   postit.appendChild(textLine);
   postit.appendChild(button);
-  todoList.appendChild(postit);
+  postContainer.style.zIndex = newTodoObj.id / 10000;
+  postContainer.appendChild(postit);
+  postContainer.classList.add("postContainer");
+  todoList.style.position = "relative";
+  postContainer.style.position = "absolute";
+  if (ifNew) {
+    console.log(initX, initY);
+    postContainer.style.top = "10px";
+    postContainer.style.left = "0px";
+  } else {
+    postContainer.style.top = "".concat(newTodoObj.yPos, "px");
+    postContainer.style.left = "".concat(newTodoObj.xPos, "px");
+  }
+  todoList.appendChild(postContainer);
+  var active = false;
+  var currentX;
+  var currentY;
+  var initialX;
+  var initialY;
+  var xOffset = 0;
+  var yOffset = 0;
+  postContainer.addEventListener("touchstart", dragStart, false);
+  postContainer.addEventListener("touchend", dragEnd, false);
+  postContainer.addEventListener("touchmove", drag, false);
+  postContainer.addEventListener("mousedown", dragStart, false);
+  postContainer.addEventListener("mouseup", dragEnd, false);
+  postContainer.addEventListener("mousemove", drag, false);
+  function dragStart(e) {
+    if (e.type === "touchstart") {
+      initialX = e.touches[0].clientX - xOffset;
+      initialY = e.touches[0].clientY - yOffset;
+    } else {
+      initialX = e.clientX - xOffset;
+      initialY = e.clientY - yOffset;
+    }
+    if (e.target === postit || e.target === textLine) {
+      active = true;
+    }
+  }
+  function dragEnd(e) {
+    initialX = currentX;
+    initialY = currentY;
+
+    // content 부분 클릭시 오차 생김
+    if (e.target === textLine) {
+      initX = e.target.getBoundingClientRect().x - 42.546875;
+      initY = e.target.getBoundingClientRect().y - (193.40625 - 168.40625);
+    } else {
+      initX = e.target.getBoundingClientRect().x;
+      initY = e.target.getBoundingClientRect().y;
+    }
+    active = false;
+  }
+  function drag(e) {
+    if (active) {
+      e.preventDefault();
+      if (e.type === "touchmove") {
+        currentX = e.touches[0].clientX - initialX;
+        currentY = e.touches[0].clientY - initialY;
+      } else {
+        currentX = e.clientX - initialX;
+        currentY = e.clientY - initialY;
+      }
+      xOffset = currentX;
+      yOffset = currentY;
+      setTranslate(currentX, currentY, postContainer);
+    }
+  }
+  function setTranslate(xPos, yPos, el) {
+    el.style.transform = "translate3d(" + xPos + "px, " + yPos + "px, 0)";
+  }
 }
 
 // submit 관리
@@ -186,17 +261,21 @@ function handleToDoSubmit(event) {
   var newTodoObj = {
     text: newTodo,
     id: Date.now(),
-    color: postitColor
+    color: postitColor,
+    xPos: initX,
+    yPos: initY
   };
   toDos.push(newTodoObj);
-  makePostit(newTodoObj);
+  makePostit(newTodoObj, true);
   saveToDos();
 }
 var savedToDos = localStorage.getItem(TODOS_KEY);
 if (savedToDos !== null) {
   var parsedToDos = JSON.parse(savedToDos);
   toDos = parsedToDos;
-  parsedToDos.forEach(makePostit);
+  parsedToDos.forEach(function (el) {
+    makePostit(el, false);
+  });
 }
 submitBtn.addEventListener("click", handleToDoSubmit);
 },{}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
@@ -224,7 +303,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60757" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53348" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
--- js/todos.js
+++ js/todos.js
@@ -11,7 +11,7 @@ let toDos = [];
 const submitRed = todoForm.querySelector(".btn-red");
 const submitBlue = todoForm.querySelector(".btn-blue");
 const submitGreen = todoForm.querySelector(".btn-green");
-let postitColor;
+let postitColor = "red";
 const icon = document.createElement("i");
 icon.setAttribute("class", "fa-solid fa-check fa-2xl");
 
@@ -40,8 +40,13 @@ function deleteTodo(event) {
   saveToDos();
 }
 
+// 좌표 저장용
+let initX = 0;
+let initY = 10;
+
 // 포스트잇 제작
-function makePostit(newTodoObj) {
+function makePostit(newTodoObj, ifNew) {
+  const postContainer = document.createElement("div");
   const postit = document.createElement("div");
   const dot = document.createElement("span");
   const textLine = document.createElement("div");
@@ -57,7 +62,90 @@ function makePostit(newTodoObj) {
   postit.appendChild(dot);
   postit.appendChild(textLine);
   postit.appendChild(button);
-  todoList.appendChild(postit);
+  postContainer.style.zIndex = newTodoObj.id / 10000;
+  postContainer.appendChild(postit);
+  postContainer.classList.add("postContainer");
+  todoList.style.position = "relative";
+  postContainer.style.position = "absolute";
+  if (ifNew) {
+    console.log(initX, initY);
+    postContainer.style.top = `10px`;
+    postContainer.style.left = `0px`;
+  } else {
+    postContainer.style.top = `${newTodoObj.yPos}px`;
+    postContainer.style.left = `${newTodoObj.xPos}px`;
+  }
+
+  todoList.appendChild(postContainer);
+
+  let active = false;
+  let currentX;
+  let currentY;
+  let initialX;
+  let initialY;
+  let xOffset = 0;
+  let yOffset = 0;
+
+  postContainer.addEventListener("touchstart", dragStart, false);
+  postContainer.addEventListener("touchend", dragEnd, false);
+  postContainer.addEventListener("touchmove", drag, false);
+
+  postContainer.addEventListener("mousedown", dragStart, false);
+  postContainer.addEventListener("mouseup", dragEnd, false);
+  postContainer.addEventListener("mousemove", drag, false);
+
+  function dragStart(e) {
+    if (e.type === "touchstart") {
+      initialX = e.touches[0].clientX - xOffset;
+      initialY = e.touches[0].clientY - yOffset;
+    } else {
+      initialX = e.clientX - xOffset;
+      initialY = e.clientY - yOffset;
+    }
+
+    if (e.target === postit || e.target === textLine) {
+      active = true;
+    }
+  }
+
+  function dragEnd(e) {
+    initialX = currentX;
+    initialY = currentY;
+
+    // content 부분 클릭시 오차 생김
+    if (e.target === textLine) {
+      initX = e.target.getBoundingClientRect().x - 42.546875;
+      initY = e.target.getBoundingClientRect().y - (193.40625 - 168.40625);
+    } else {
+      initX = e.target.getBoundingClientRect().x;
+      initY = e.target.getBoundingClientRect().y;
+    }
+
+    active = false;
+  }
+
+  function drag(e) {
+    if (active) {
+      e.preventDefault();
+
+      if (e.type === "touchmove") {
+        currentX = e.touches[0].clientX - initialX;
+        currentY = e.touches[0].clientY - initialY;
+      } else {
+        currentX = e.clientX - initialX;
+        currentY = e.clientY - initialY;
+      }
+
+      xOffset = currentX;
+      yOffset = currentY;
+
+      setTranslate(currentX, currentY, postContainer);
+    }
+  }
+
+  function setTranslate(xPos, yPos, el) {
+    el.style.transform = "translate3d(" + xPos + "px, " + yPos + "px, 0)";
+  }
 }
 
 // submit 관리
@@ -70,9 +158,11 @@ function handleToDoSubmit(event) {
     text: newTodo,
     id: Date.now(),
     color: postitColor,
+    xPos: initX,
+    yPos: initY,
   };
   toDos.push(newTodoObj);
-  makePostit(newTodoObj);
+  makePostit(newTodoObj, true);
   saveToDos();
 }
 
@@ -81,7 +171,8 @@ const savedToDos = localStorage.getItem(TODOS_KEY);
 if (savedToDos !== null) {
   const parsedToDos = JSON.parse(savedToDos);
   toDos = parsedToDos;
-  parsedToDos.forEach(makePostit);
+  parsedToDos.forEach((el) => {
+    makePostit(el, false);
+  });
 }
-
 submitBtn.addEventListener("click", handleToDoSubmit);
